84655fbd6ec5efe923363e3a068eb12baea8e542
jds375 <jds375@cornell.edu>
2014-03-21 00:27:41 -0400

    Final changes.

--- Array.py
+++ Array.py
@@ -5,17 +5,12 @@ maximum number. Solution should be less than O(n). The array is
 non-empty. Ex - {1,2,3,4,5,3,1}
 """
 def ternarySearch(a):
-    if len(a) == 1:
-        return a[0]
-    elif len(a) == 2:
-        return max(a[0], a[1])
-    else:
-        return rTernarySearch(a, (len(a)-1)/3, 2*(len(a)-1)/3, 0, len(a)-1)
+    return rTernarySearch(a, (len(a)-1)/3, 2*(len(a)-1)/3, 0, len(a)-1)
 
 def rTernarySearch(a, s, t, l, r):
     if s == t:
         return a[s]
     elif a[s] <= a[t]:
-        return rTernarySearch(a, s+1, r, s+1, r)
+        return rTernarySearch(a, (s+1)+((r-s+1)/3), (s+1)+(2*(r-s+1)/3), s+1, r)
     else:
-        return rTernarySearch(a, l, t-1, l, t-1)
+        return rTernarySearch(a, l+((t-l)/3), l+(2*(t-l)/3), l, t-1)
